16d5a6fe2a0b1c9679601a8410580d0af5e21617
陈健华 <jianhuachen@kugou.net>
2017-02-06 14:33:42 +0800

    哈哈哈哈

--- app/src/main/java/app/jyh/com/windowdemo/MainActivity.java
+++ app/src/main/java/app/jyh/com/windowdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package app.jyh.com.windowdemo;
 
+import android.app.Service;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
@@ -10,16 +12,28 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
 	private WindowManager.LayoutParams param;
 	private WindowManager mWindowManager;
+	Vibrator vibrator;
 
 	private View v;
 	private ProgressBar progressBar;
 	private TextView textView;
-	private STATUS status = STATUS.READY;
+	private TextView resetView;
+	private TextView timeView;
+	private TextView exitView;
 
 	DisplayMetrics dm;
 
@@ -30,20 +44,32 @@ public class MainActivity extends AppCompatActivity {
 		v = LayoutInflater.from(getApplicationContext()).inflate(R.layout.progress, null, false);
 		progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
 		progressBar.setProgress(0);
+		resetView = (TextView) v.findViewById(R.id.reset);
 		textView = (TextView) v.findViewById(R.id.tv);
 		textView.setText(status.getDesc());
+		timeView = (TextView) v.findViewById(R.id.time);
+		exitView = (TextView) findViewById(R.id.exit);
+		exitView.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+		textView.setOnClickListener(new TimerClickListener());
+		resetView.setOnClickListener(new ResetClickListener());
 
 		dm = new DisplayMetrics();
-
 		getWindowManager().getDefaultDisplay().getMetrics(dm);
 
+		vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
 		mWindowManager = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
 		param = new WindowManager.LayoutParams();
-		param.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;     // 系统提示类型,重要
+		param.type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
 		param.format = 1;
-		param.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE; // 不能抢占聚焦点
+		param.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE;
 		param.flags = param.flags | WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH;
-		param.flags = param.flags | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS; // 排版不受限制
+		param.flags = param.flags | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS;
 
 		param.alpha = 1.0f;
 
@@ -60,9 +86,124 @@ public class MainActivity extends AppCompatActivity {
 		mWindowManager.addView(v, param);
 	}
 
+	private STATUS status = STATUS.READY;
+	private long mCurrentTimeStamp = 0L;
+	private Disposable disposable;
+	private static long MAX_TIME_STAMP = 30 * 1000;
+	private static final int interval = 10;
+	private DecimalFormat format = new DecimalFormat("#.00");
+
+	private class TimerClickListener implements View.OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			if (mCurrentTimeStamp >= MAX_TIME_STAMP) {
+				Toast.makeText(MainActivity.this, "计时已经完成", Toast.LENGTH_SHORT).show();
+			}
+			if (status == STATUS.READY) {
+				//就绪状态，点击后开始计时
+				mCurrentTimeStamp = 0L;
+				progressBar.setProgress(0);
+				status = STATUS.RUNNING;
+				textView.setText(status.getDesc());
+				disposable = Observable.interval(interval, TimeUnit.MILLISECONDS)
+						.observeOn(AndroidSchedulers.mainThread())
+						.subscribe(new Consumer<Long>() {
+							@Override
+							public void accept(Long aLong) throws Exception {
+								mCurrentTimeStamp += interval;
+								long v = mCurrentTimeStamp * 100 / MAX_TIME_STAMP;
+								progressBar.setProgress(v > 100 ? 100 : (int) v);
+								timeView.setText(format.format(((float) mCurrentTimeStamp) / 1000));
+								if (mCurrentTimeStamp >= MAX_TIME_STAMP) {
+									if (disposable != null && !disposable.isDisposed()) {
+										disposable.dispose();
+									}
+									status = STATUS.READY;
+									textView.setText(status.getDesc());
+									mCurrentTimeStamp = 0L;
+									progressBar.setProgress(0);
+									vibrator.vibrate(500);
+								}
+							}
+						}, new Consumer<Throwable>() {
+							@Override
+							public void accept(Throwable throwable) throws Exception {
+								throwable.printStackTrace();
+							}
+						});
+			} else if (status == STATUS.PAUSE) {
+				//暂停状态，点击后继续计时
+				status = STATUS.RUNNING;
+				textView.setText(status.getDesc());
+				disposable = Observable.interval(interval, TimeUnit.MILLISECONDS)
+						.observeOn(AndroidSchedulers.mainThread())
+						.subscribe(new Consumer<Long>() {
+							@Override
+							public void accept(Long aLong) throws Exception {
+								mCurrentTimeStamp += interval;
+								long v = mCurrentTimeStamp * 100 / MAX_TIME_STAMP;
+								progressBar.setProgress(v > 100 ? 100 : (int) v);
+								timeView.setText(format.format(((float) mCurrentTimeStamp) / 1000));
+								if (mCurrentTimeStamp >= MAX_TIME_STAMP) {
+									if (disposable != null && !disposable.isDisposed()) {
+										disposable.dispose();
+									}
+									status = STATUS.READY;
+									textView.setText(status.getDesc());
+									mCurrentTimeStamp = 0L;
+									progressBar.setProgress(0);
+									vibrator.vibrate(500);
+								}
+							}
+						}, new Consumer<Throwable>() {
+							@Override
+							public void accept(Throwable throwable) throws Exception {
+								throwable.printStackTrace();
+							}
+						});
+			} else if (status == STATUS.RUNNING) {
+				//正在运行状态，点击后暂停计时
+				status = STATUS.PAUSE;
+				textView.setText(status.getDesc());
+				if (disposable != null && !disposable.isDisposed()) {
+					disposable.dispose();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void finish() {
+		try {
+			if (mWindowManager != null && v != null) {
+				mWindowManager.removeView(v);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		super.finish();
+	}
+
+	private class ResetClickListener implements View.OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			//点击后直接重置
+			status = STATUS.READY;
+			textView.setText(status.getDesc());
+			mCurrentTimeStamp = 0L;
+			progressBar.setProgress(0);
+			timeView.setText(format.format(((float) mCurrentTimeStamp) / 1000));
+			if (disposable != null && !disposable.isDisposed()) {
+				disposable.dispose();
+			}
+		}
+	}
+
 	private enum STATUS {
 
-		READY("开始"), PAUSE("继续"), RUNNING("暂停"), COMPLETE("重置");
+		READY("开始"), PAUSE("继续"), RUNNING("暂停");
 
 		private String desc;
 
